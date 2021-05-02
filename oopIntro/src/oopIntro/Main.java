faa8ec4f96c3d836e616a890aace19e11d703d44
mmustafaseker <mustiseker.06@gmail.com>
2021-05-02 16:45:30 +0300

    oopIntro

---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,31 @@
+package oopIntro;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Product product1 = new Product(1,"Lenovo V14",15000,"6gb Ram",10,13500); // Örnek oluþturma, referans oluþturma, instance
+
+		
+		Product product2 = new Product();
+		product2.setId(2);
+		product2.setName("Lenovo 15");
+		product2.setDetail("16 gb ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
+		
+		System.out.println(product2.getUnitPriceAfterDiscount());
+		
+		Category category1 = new Category();
+		category1.setId(1);
+		category1.setName("Yiyecek");
+		
+		Category category2 = new Category();
+		category2.setId(2);
+		category2.setName("Ýçecek");
+
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
+	}
+
+}
